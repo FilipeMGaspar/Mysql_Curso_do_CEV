e789435cdc7ac519f19daa497a2102394a338d64
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-11 19:45:06 +0000

    Aula 4 - Melhorando a Estrutura do Banco de Dados

--- Aula_04-Melhorando_a_Estrutura_do_Banco_de_Dados.sql
+++ Aula_04-Melhorando_a_Estrutura_do_Banco_de_Dados.sql
@@ -5,10 +5,12 @@ default collate utf8_general_ci;
 use cadastro;
 
 create table pessoas (
+	id int not null auto_increment,
 	nome varchar(30) not null,
     nascimento date,
     sexo enum('M', 'F'),
     peso decimal(5,2),
     altura decimal(3,2),
-    nacionalidade varchar(20) default 'Português'
+    nacionalidade varchar(20) default 'Português',
+    primary key (id)
 ) default charset = utf8;
