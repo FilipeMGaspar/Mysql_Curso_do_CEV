535221b2cb2c5e54de125f0774b191089da382b6
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-12 16:28:49 +0000

    Aula 5 - Inserindo Dados na Tabela (INSERT INTO)

--- Aula05 - Inserindo Dados na Tabela (INSERT INTO).sql
+++ Aula05 - Inserindo Dados na Tabela (INSERT INTO).sql
@@ -6,3 +6,8 @@ values
 ('Godofredo', '1984-01-02', 'M', '78.5', '1.8', 'Brasil');
 
 select * from pessoas;
+
+insert into pessoas
+(nome, nascimento, sexo, peso, altura, nacionalidade)
+values 
+('Maria', '1999-12-20', 'f', '48.5', '1.2', 'Portugal');
