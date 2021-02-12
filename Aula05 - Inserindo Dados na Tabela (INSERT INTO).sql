7134b72ea697e0db35b33747c300497f1d303c48
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-12 16:32:21 +0000

    Aula 5 - Inserindo Dados na Tabela (INSERT INTO)

--- Aula05 - Inserindo Dados na Tabela (INSERT INTO).sql
+++ Aula05 - Inserindo Dados na Tabela (INSERT INTO).sql
@@ -1,3 +1,22 @@
+drop database cadastro;
+
+create database cadastro 
+default character set utf8
+default collate utf8_general_ci;
+
+use cadastro;
+
+create table pessoas (
+    id int not null auto_increment,
+    nome varchar(30) not null,
+    nascimento date,
+    sexo enum('M', 'F'),
+    peso decimal(5,2),
+    altura decimal(3,2),
+    nacionalidade varchar(20) default 'Português',
+    primary key (id)
+) default charset = utf8;
+
 use cadastro;
 
 insert into pessoas
@@ -11,3 +30,5 @@ insert into pessoas
 (nome, nascimento, sexo, peso, altura, nacionalidade)
 values 
 ('Maria', '1999-12-20', 'f', '48.5', '1.2', 'Portugal');
+
+select * from pessoas;
