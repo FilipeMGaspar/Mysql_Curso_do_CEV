dfac56fedc0762648f89911b153e90d86ab26f43
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-14 16:42:37 +0000

    Aula 7 - Manipulando Linhas (UPDATE, DELETE e TRUNCATE)

--- Aula_7.sql
+++ Aula_7.sql
@@ -2,3 +2,12 @@ use cadastro;
 
 select * from gafanhotos;
 
+select * from cursos;
+
+insert into cursos values
+('1', 'HTML4', 'Curso de HTML5', '40', '37', '2014'),
+('2', 'Algoritmos', 'Lógica de Programação', '20', '15', '2014'),
+('3', 'Photoshop', 'Dicas de photoshop CC', '10', '8', '2014'),
+('4', 'PGP', 'Curso de PHP para iniciantes', '40', '20', '2010'),
+('5', 'Jarva', 'Intrudoção à Liguangem Java', '10', '29', '2000'),
+('5', 'Jarva', 'Intrudoção à Liguangem Java', '10', '29', '2000'),
