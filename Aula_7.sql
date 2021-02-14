b53fed4aef18eee408d0a5eebc4e2d6e64badf22
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-14 17:09:51 +0000

    Aula 7 - Manipulando Linhas (UPDATE, DELETE e TRUNCATE)

--- Aula_7.sql
+++ Aula_7.sql
@@ -30,4 +30,6 @@ update cursos set ano = '2018', carga = '0' where ano = '2050' limit 1;
 
 delete from cursos where idcurso = '8';
 
+delete from cursos where ano = '2050' limit 2;
+
 select * from cursos;
