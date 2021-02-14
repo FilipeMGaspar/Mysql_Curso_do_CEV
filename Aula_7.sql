5a1e1140d29c9806e94645021c0229d7be27163c
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-14 16:59:09 +0000

    Aula 7 - Manipulando Linhas (UPDATE, DELETE e TRUNCATE)

--- Aula_7.sql
+++ Aula_7.sql
@@ -21,3 +21,5 @@ select * from cursos;
 update cursos set nome = 'HTML5' where idcurso = '1';
 
 update cursos set nome = 'PHP', ano = '2015' where idcurso = '4';
+
+update cursos set nome = 'Java', ano = '2015', carga = '40' where idcurso = '5' limit 1;
