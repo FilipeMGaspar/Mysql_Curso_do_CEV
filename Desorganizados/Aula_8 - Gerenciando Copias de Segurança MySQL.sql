030cbe0092b009930e79cfea78a71b4cd8498072
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-15 14:25:10 +0000

    Aula 8 - Gerenciando Cópias de Segurança MySQL

--- Desorganizados/Aula_8 - Gerenciando Copias de Segurança MySQL.sql
+++ Desorganizados/Aula_8 - Gerenciando Copias de Segurança MySQL.sql
@@ -1,3 +1,12 @@
 use cadastro;
 
 select * from cursos;
+
+insert into cursos values
+('1', 'HTML5', 'Curso de HTML5', '40', '37', '2014'),
+('2', 'Algoritmos', 'Lógica de Programação', '20', '15', '2014'),
+('3', 'Photoshop', 'Dicas de photoshop CC', '10', '8', '2014'),
+('4', 'PHP', 'Curso de PHP para iniciantes', '40', '20', '2015'),
+('5', 'Java', 'Intrudoção à Liguangem Java', '40', '29', '2015'),
+('6', 'MySQL', 'Bancos de Dados MySQL', '30', '15', '2016'),
+('7', 'Word', 'Curso completo de Word', '40', '30', '2016');
