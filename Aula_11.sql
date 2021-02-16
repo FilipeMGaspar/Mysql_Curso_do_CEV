0ec426742af7895974062a0a0595eacf4c2d656a
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-16 16:56:22 +0000

    Aula 11 - SELECT (Parte 1)

--- Aula_11.sql
+++ Aula_11.sql
@@ -15,3 +15,5 @@ select ano, nome, carga from cursos order by ano;
 select ano, nome, carga from cursos order by ano, nome;
 
 select * from cursos where ano = '2016' order by nome;
+
+select nome, carga from cursos where ano = '2016' order by nome;
