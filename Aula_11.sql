cbfcc7ad4180c42051bfbc21d88add8510a6987a
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-16 16:48:56 +0000

    Aula 11 - SELECT (Parte 1)

--- Aula_11.sql
+++ Aula_11.sql
@@ -7,3 +7,8 @@ select * from cursos;
 select * from cursos order by nome;
 
 select * from cursos order by nome desc;
+
+select * from cursos order by nome asc;
+
+select nome, carga, ano from cursos order by nome;
+
