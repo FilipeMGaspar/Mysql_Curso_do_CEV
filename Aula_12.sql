6fbf3029db5b12208bacfb455b2b204a345d1965
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-17 17:04:34 +0000

    Aula 12 - SELECT (Parte 2)

--- Aula_12.sql
+++ Aula_12.sql
@@ -7,3 +7,10 @@ select * from cursos where nome like 'P%';
 select * from cursos where nome like '%A';
 
 select * from cursos where nome like '%A%';
+
+select * from cursos where nome not like '%A%';
+
+/*update cursos set nome = 'PáOO' where idCurso = '9';*/
+/*select * from cursos where nome not like '%A%';*/
+
+select * from cursos where nome like 'PH%P';
