90c0dc81a2d879c194f318547306699d15457df2
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-17 17:20:41 +0000

    Aula 12 - SELECT (Parte 2)

--- Aula_12.sql
+++ Aula_12.sql
@@ -25,4 +25,16 @@ select * from gafanhotos;
 
 select * from gafanhotos where nome like '%silva%';
 
+select carga from cursos;
+
 select distinct carga from cursos;
+
+select nacionalidade from gafanhotos;
+
+select distinct nacionalidade from gafanhotos order by nacionalidade;
+
+select * from cursos;
+
+select count(*) from cursos;
+
+select * from cursos where carga > 30;
