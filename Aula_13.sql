47f5535400f04eacbfa2818136d0475faebe620f
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-18 15:53:36 +0000

    Aula 13 - SELECT (Parte 3)

--- Aula_13.sql
+++ Aula_13.sql
@@ -3,3 +3,9 @@ use cadastro;
 select carga from cursos group by carga;
 
 select carga, count(nome) from cursos group by carga;
+
+select * from cursos where totAulas = 30;
+
+select * from cursos where totAulas > 20;
+
+select carga, count(nome) from cursos where totAulas > 20 group by carga;
