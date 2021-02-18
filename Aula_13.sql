e7167d45c87c2c1aefae2a75e58e57cf5c29e8cb
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-18 16:09:32 +0000

    Aula 13 - SELECT (Parte 3)

--- Aula_13.sql
+++ Aula_13.sql
@@ -17,3 +17,11 @@ select ano, count(*) from cursos group by ano having count(ano) >= 2 order by co
 select ano, count(*) from cursos group by ano having ano > 2013 order by count(*) desc;
 
 select ano, count(*) from cursos where totAulas > 30 group by ano having ano > 2013 order by count(*) desc;
+
+select avg(carga) from cursos;
+
+select * from cursos where ano > 2013;
+
+
+/*
+*/
