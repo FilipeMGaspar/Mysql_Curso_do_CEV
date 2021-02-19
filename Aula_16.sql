098f6926af6c1d0559b1e26969d84f2be2045331
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-19 14:01:50 +0000

    Aula 16 - INNER JOIN com várias tabelas

---
+++ Aula_16.sql
@@ -0,0 +1,11 @@
+use cadastro;
+
+create table gafanhoto_assiste_curso( 
+	id int not null auto_increment,
+    data date,
+    idcurso int,
+    idgafanhoto int,
+    primary key (id),
+    foreign key (idgafanhoto) references gafanhotos(id),
+    foreign key (idcurso) references cursos(idCurso)
+) default charset = utf8;
