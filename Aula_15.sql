456579155656ee0c0e50b0505ff28cfdefdc71a7
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-19 13:33:51 +0000

    Aula 15 - Chaves Estrangeiras e JOIN

--- Aula_15.sql
+++ Aula_15.sql
@@ -31,6 +31,5 @@ select nome, cursoperferido from gafanhotos;
 
 select nome, ano from cursos;
 
-select gafanhotos.nome, cursos.nome, cursos.ano 
-from  gafanhotos join cursos on cursos.idCurso = gafanhotos.cursoperferido;
+select gafanhotos.nome, cursos.nome, cursos.ano from  gafanhotos join cursos on cursos.idCurso = gafanhotos.cursoperferido;
 
