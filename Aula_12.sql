3668c65be4baaf8cbf59450df9e172d653561963
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-21 17:42:23 +0000

    Aula 12 - SELECT (Parte 2) - Exercicios

--- Aula_12.sql
+++ Aula_12.sql
@@ -68,6 +68,8 @@ select nome from gafanhotos where sexo='F';
     aqueles que nasceram entre 1/jan/2000 
     e 31/dez/2015
 */
+select * from gafanhotos where nascimento between '2000-01-01' and '2015-12-31';
+
 
 /*Exercicio 3
 	uma lista com o nome de todos
