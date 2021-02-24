754da4b33840e972615868a541b6d94b1de04ce8
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2021-02-24 21:23:36 +0000

    Aula 13 - SELECT (Parte 3) - Exercicios

--- Aula_13.sql
+++ Aula_13.sql
@@ -50,5 +50,4 @@ pesam mais de 100Kg e que estão acima
 da média de altura de todos os 
 cadastro
 */
-select nome, altura, count(altura) from gafanhotos group by altura having altura > (select avg(altura) from gafanhotos where peso > 100);
-select * from gafanhotos;
+select peso, count(altura) from gafanhotos where peso > 100 group by altura having altura > (select avg(altura) from gafanhotos);
